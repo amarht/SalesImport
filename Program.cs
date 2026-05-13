8340f5018e210d8e6ed55ceca2f4db2f63125fe7
Amar <amarhasantawfiq@outlook.com>
2026-05-13 12:27:33 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Globalization;
+
+public static class CsvStreamReader
+{
+    public static IEnumerable<Sale> ReadSales(string path)
+    {
+        using var reader = new StreamReader(
+            path,
+            System.Text.Encoding.UTF8,
+            detectEncodingFromByteOrderMarks: true,
+            bufferSize: 1 << 20
+        );
+
+        // Skip header
+        reader.ReadLine();
+
+        string? line;
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            var parts = line.Split(',');
+
+            yield return new Sale
+            {
+                SaleId = int.Parse(parts[0]),
+
+                StoreCode = parts[1],
+
+                ProductCode = parts[2],
+
+                Quantity = int.Parse(parts[3]),
+
+                UnitPrice = float.Parse(
+                    parts[4],
+                    CultureInfo.InvariantCulture
+                ),
+
+                SaleDate = DateTime.ParseExact(
+                    parts[5],
+                    "yyyy-MM-dd",
+                    CultureInfo.InvariantCulture
+                )
+            };
+        }
+    }
+}
+
+class Program {
+    public static void Main() {
+        var salesCsv = CsvStreamReader.ReadSales("Sales.csv");
+        
+        foreach (var sale in salesCsv) {
+            Console.WriteLine(sale.StoreCode);
+        }
+    }
+}
---
+++ Sale.cs
@@ -0,0 +1,8 @@
+public class Sale {
+    public int SaleId;
+    public string StoreCode;
+    public string ProductCode;
+    public int Quantity;
+    public float UnitPrice;
+    public DateTime SaleDate;
+}
